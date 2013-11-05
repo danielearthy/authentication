aadded7bc6100139ae2aaaf3eda338a854cd016d
Daniel Earthy <danielearthy@gmail.com>
2013-11-04 19:30:59 -0800

    final

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,31 +1,43 @@
+enable :sessions
 get '/' do
-  # render home page
- #TODO: Show all users if user is signed in
-  erb :index
+  if session[:user_id]
+    @users = User.all
+    erb :index
+  else
+    redirect '/sessions/new'
+  end
 end
 
 #----------- SESSIONS -----------
 
 get '/sessions/new' do
-  # render sign-in page 
   erb :sign_in
 end
 
 post '/sessions' do
-  # sign-in
+  @user = User.find_by email: params[:email]
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/sessions/new'
+  end
 end
 
-delete '/sessions/:id' do
-  # sign-out -- invoked via AJAX
+delete '/sessions/logout' do
+  session.clear
 end
 
 #----------- USERS -----------
 
 get '/users/new' do
-  # render sign-up page
   erb :sign_up
 end
 
 post '/users' do
-  # sign-up a new user
+  p params
+  @user = User.create(params[:user])
+  session[:user_id] = @user.id
+  redirect '/'
+  
 end
